0ffd6404472235c4c52b48a649d4b029dfe600ef
jonetheduckjones89-boop <jonetheduckjones89@gmail.com>
2025-12-20 16:40:42 +0500

    Delete routes directory

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inquiryController = require('../controllers/inquiryController');
-
-// Using /submit to match production hotfix requirements
-router.post('/submit', inquiryController.handleInquiry);
-
-module.exports = router;
